cfe7a4b9b97ea90310a18ed9bb1b9c1e3dd084e0
Krishna Chaudhari <krishnachaudhari2309@gmail.com>
2023-10-30 17:48:43 +0530

    2nd commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-function whitename(event) {
-    event.add
-}
+const mobileMenuButton = document.getElementById("mobile-menu");
+const navbarLinks = document.getElementById("navbar-links");
+
+mobileMenuButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("show");
+});

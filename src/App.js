23fff1b3ae14a6eb0d3b68367d46e647d748a5b2
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2024-04-29 23:48:01 +0530

    added gallery part

--- src/App.js
+++ src/App.js
@@ -1,28 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import Home from './Components/Home';
-// import { Route, Routes } from 'react-router-dom';
 
-
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home/>} />
-//       {/* <Route path='login' element={<LoginRoute />} />
-//       <Route path='register' element={<RegisterRoute />} />
-//       <Route path='profile' element={<Profile/>}/>
-//       <Route path='xxxx' element={<DashboardRoute />} />
-//       <Route path='otp' element={<OtpRoute/>} />
-//       <Route path='forgotpassword' element={<FpForm/>}/>
-//       <Route path='poster' element={<Poster />} />
-//       <Route path='dashboard' element= {<DashboardRoute />}/>
-//       <Route path='payment' element={<PaymentRoute/>}/>
-//       <Route path='player' element={<Video/>}/> */}
-//     </Routes>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home.jsx';
@@ -30,6 +6,8 @@ import About from './Components/About/About.jsx';
 import Event from './Components/Events/Event.jsx';
 import Gallery from './Components/Gallery/Gallery.jsx';
 import Team from './Components/Team/Team.jsx';
+import PhotoScreen from './Components/Gallery/PhotoScreen.jsx';
+
 // import About from './components/About';
 
 const App = () => {
@@ -41,6 +19,7 @@ const App = () => {
         <Route path="/events" element={<Event/>} />
         <Route path="/gallery" element={<Gallery/>} />
         <Route path="/team" element={<Team/>} />
+        <Route path='/photos' element={<PhotoScreen/>} />
       </Routes>
     </Router>
   );
--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
+import data from './Gallery.json';
+import EventCard from './EventCard';
+import "./Gallery.css"
+import ganesh from "../../assests/images/Gallery/GaneshUtsav/3.jpg"
+import shiv from "../../assests/images/Gallery/Shivjayanti/13.jpg"
+import arbh from "../../assests/images/Gallery/Aarambh/8.webp"
+import bhr from "../../assests/images/Gallery/Bharari/8.jpg"
+import gen from "../../assests/images/Gallery/General/Trip/9.jpg"
+
+
+
 
 const Gallery = () => {
+
   return (
     <>
     <Navbar/>
-    <h1>Gallery</h1>
+    <div className="gallery">
+      <div className="g-screen">
+        <h1>Our Events</h1>
+
+        <div className="all-events">
+          <EventCard
+          name = 'GaneshUtsav'
+          image = {ganesh}
+          event = "GaneshUtsav"
+          />
+          <EventCard
+          name = 'Shiv Jayanti'
+          image = {shiv}
+          event = "Shivjayanti"
+          />
+          <EventCard
+          name = 'Aarambh'
+          image = {arbh}
+          event = "Aarambh"
+          />
+          <EventCard
+          name = 'Bharari'
+          image = {bhr}
+          event = "Bharari"
+          />
+          <EventCard
+          name = 'Food Fest'
+          image = ''
+          event = "FoodFest"
+          />
+          <EventCard
+          name = 'Misal Paw Party'
+          image = ''
+          event = "Misal Paw Party"
+          />
+          <EventCard
+          name = 'General'
+          image = {gen}
+          event = "General"
+          />
+          
+
+        </div>
+      </div>
+    </div>
     <Footer/>
     </>
   )
